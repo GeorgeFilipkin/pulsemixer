91f208f63722bc7608c86732f078a0c572de327f
George <george@zenbook>
2016-09-20 22:59:24 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pulsemixer',
+      version='1.2.0',
+      description='pulsemixer',
+      url='https://github.com/GeorgeFilipkin/pulsemixer',
+      author='George Filipkin',
+      author_email='botebotebot@gmail.com',
+      license='MIT',
+      scripts=['pulsemixer'],
+      )
+
