71f15d0a04b096096f2f6440769048ab466a8999
George <george@zenbook>
2017-09-19 14:21:26 +0300

    version bump

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='pulsemixer',
-      version='1.2.0',
-      description='pulsemixer',
+      version='1.3.0',
+      description='pulsemixer - cli and curses mixer for pulseaudio',
       url='https://github.com/GeorgeFilipkin/pulsemixer',
       author='George Filipkin',
       author_email='botebotebot@gmail.com',
