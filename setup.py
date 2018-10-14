6b1e2628eb1592dd8fb3af1482966461b45053bd
GeorgeFilipkin <botebotebot@gmail.com>
2018-10-14 13:18:04 +0300

    pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pulsemixer',
-      version='1.4.0',
-      description='pulsemixer - cli and curses mixer for pulseaudio',
-      url='https://github.com/GeorgeFilipkin/pulsemixer',
-      author='George Filipkin',
-      author_email='botebotebot@gmail.com',
-      license='MIT',
-      scripts=['pulsemixer'],
-      )
+    version='1.4.0',
+    description='pulsemixer - cli and curses mixer for pulseaudio',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/GeorgeFilipkin/pulsemixer',
+    author='George Filipkin',
+    author_email='botebotebot@gmail.com',
+    license='MIT',
+    scripts=['pulsemixer'],
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Console :: Curses',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Sound/Audio :: Mixers',
+        'Topic :: Utilities',
+    ],
+)
 
